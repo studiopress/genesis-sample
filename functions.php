f1fa8b500224c6a25c61445e2e3c5ce3c0ac306c
Brian Gardner <bmg1227@gmail.com>
2013-01-07 19:14:59 -0600

    Initial push of theme files.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+// Start the engine
+require_once( get_template_directory() . '/lib/init.php' );
+
+// Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+
+// Add Viewport meta tag for mobile browsers
+add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
+function sample_viewport_meta_tag() {
+	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+}
+
+// Add support for custom background
+add_theme_support( 'custom-background' );
+
+// Add support for custom header
+add_theme_support( 'genesis-custom-header', array(
+	'width' => 1152,
+	'height' => 120
+) );
+
+// Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
