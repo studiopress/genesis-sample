52c8c7b332bfd39fc3e535d64af07b9f2bd6a456
Nathan Rice <ncrice@gmail.com>
2013-05-29 12:17:16 -0300

    Sync with local from @bgardner

--- functions.php
+++ functions.php
@@ -7,17 +7,20 @@ define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.0-beta1' );
 
-//* Add Viewport meta tag for mobile browsers
+//* Enqueue Lato Google font
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
+	wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=Lato:700', array(), PARENT_THEME_VERSION );
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'genesis-html5' );
+
+//* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
 //* Add support for custom background
 add_theme_support( 'custom-background' );
-
-//* Add support for custom header
-add_theme_support( 'custom-header', array(
-	'width'  => 1152,
-	'height' => 120
-) );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
