ec2fcfe763f9aa3f525b21551ba10985013deba9
Nathan Rice <ncrice@gmail.com>
2013-05-28 20:42:24 -0400

    Sync with Genesis 2.0.0 develop.

--- functions.php
+++ functions.php
@@ -5,20 +5,17 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '1.9.2' );
+define( 'CHILD_THEME_VERSION', '2.0.0-beta1' );
 
 //* Add Viewport meta tag for mobile browsers
-add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
-function sample_viewport_meta_tag() {
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
-}
+add_theme_support( 'genesis-responsive-viewport' );
 
 //* Add support for custom background
 add_theme_support( 'custom-background' );
 
 //* Add support for custom header
 add_theme_support( 'custom-header', array(
-	'width' => 1152,
+	'width'  => 1152,
 	'height' => 120
 ) );
 
