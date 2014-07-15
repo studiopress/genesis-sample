5141ba8b2537b84743612475d2ac5a0874eca00d
Nathan Rice <ncrice@gmail.com>
2014-07-15 15:20:36 -0400

    Sync with Genesis 2.1.2.

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.1.1' );
+define( 'CHILD_THEME_VERSION', '2.1.2' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
