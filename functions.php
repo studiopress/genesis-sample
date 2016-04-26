7f1fa0994ac9bf9cc9a685d2e4daad90f2315bc5
Lauren Mancke <lauren@copyblogger.com>
2016-04-26 09:46:51 -0400

    Revert "2.2.3 Style Updates"

--- functions.php
+++ functions.php
@@ -8,7 +8,15 @@ define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.2.2' );
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+=======
+//* Enqueue Scripts and Styles
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+>>>>>>> parent of 7b5feb4... 2.2.3 Style Updates
 	wp_enqueue_style( 'dashicons' );
 =======
 //* Enqueue Google Fonts
@@ -35,9 +43,6 @@ add_theme_support( 'custom-background' );
 <<<<<<< HEAD
 //* Add support for after entry widget
 add_theme_support( 'genesis-after-entry-widget-area' );
-
-//* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 3 );
 
 //* Add Image Sizes
 add_image_size( 'featured-image', 720, 400, TRUE );
