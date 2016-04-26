02cec9bdc1695e8c6e97f6708e4713736e97f51a
Lauren Mancke <lauren@copyblogger.com>
2016-04-26 10:29:33 -0400

    Genesis Sample: Add Accessibility Mobile Menu #20

--- functions.php
+++ functions.php
@@ -29,6 +29,14 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+	$output = array(
+		'mainMenu' => __( 'Menu', 'genesis-sample' ),
+		'subMenu'  => __( 'Menu', 'genesis-sample' ),
+	);
+	wp_localize_script( 'genesis-sample-responsive-menu', 'genesisSampleL10n', $output );
 
 }
 
