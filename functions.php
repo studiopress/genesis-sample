013077559fa6abd3f0c417cc7f3b0b7d902461c8
Lauren Mancke <lauren@copyblogger.com>
2016-04-26 10:01:27 -0400

    Revert Add version number #27

--- functions.php
+++ functions.php
@@ -7,26 +7,9 @@ define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.2.2' );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
-=======
-//* Enqueue Scripts and Styles
-add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
-function genesis_sample_enqueue_scripts_styles() {
-
-<<<<<<< HEAD
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
->>>>>>> parent of 7b5feb4... 2.2.3 Style Updates
-=======
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700,900', array(), CHILD_THEME_VERSION );
->>>>>>> parent of b3c719e... Genesis Sample Style Updates
-	wp_enqueue_style( 'dashicons' );
-=======
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
->>>>>>> parent of 9a593fe... 2.2.3 Genesis Sample Theme Changes
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
 
@@ -44,52 +27,5 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Add support for custom background
 add_theme_support( 'custom-background' );
 
-<<<<<<< HEAD
-//* Add support for after entry widget
-add_theme_support( 'genesis-after-entry-widget-area' );
-
-//* Add Image Sizes
-add_image_size( 'featured-image', 680, 400, TRUE );
-
-//* Rename primary and secondary navigation menus
-add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
-
-//* Reposition the secondary navigation menu
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
-
-//* Reduce the secondary navigation menu to one level depth
-add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
-function genesis_sample_secondary_menu_args( $args ) {
-
-	if ( 'secondary' != $args['theme_location'] ) {
-		return $args;
-	}
-
-	$args['depth'] = 1;
-
-	return $args;
-
-}
-
-//* Modify size of the Gravatar in the author box
-add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
-function genesis_sample_author_box_gravatar( $size ) {
-
-	return 180;
-
-}
-
-//* Modify size of the Gravatar in the entry comments
-add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
-function genesis_sample_comments_gravatar( $args ) {
-
-	$args['avatar_size'] = 120;
-
-	return $args;
-
-}
-=======
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
->>>>>>> parent of 9a593fe... 2.2.3 Genesis Sample Theme Changes
