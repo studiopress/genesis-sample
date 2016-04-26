64067ecb921444cc8fe47fc2d91ccf39f300d67e
Lauren Mancke <lauren@copyblogger.com>
2016-04-26 10:52:32 -0400

    Genesis Sample: Move Secondary Navigation to the Footer #35

--- functions.php
+++ functions.php
@@ -73,6 +73,27 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 //* Add Image Sizes
 add_image_size( 'featured-image', 720, 400, TRUE );
 
+//* Rename primary and secondary navigation menus
+add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+//* Reposition the secondary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+//* Reduce the secondary navigation menu to one level depth
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+
+	return $args;
+
+}
+
 //* Modify size of the Gravatar in the author box
 add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
 function genesis_sample_author_box_gravatar( $size ) {
