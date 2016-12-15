64bfa9210d29959f2e6fd33c8f995e55d51c97ec
Calvin Koepke <hello@calvinkoepke.com>
2016-12-15 12:34:54 -0700

    Update Doc Standards + Additional WooCommerce Support

--- lib/helper-functions.php
+++ lib/helper-functions.php
@@ -46,10 +46,9 @@ function genesis_sample_customizer_get_default_accent_color() {
 function genesis_sample_color_contrast( $color ) {
 
 	$hexcolor = str_replace( '#', '', $color );
-
-	$red   = hexdec( substr( $hexcolor, 0, 2 ) );
-	$green = hexdec( substr( $hexcolor, 2, 2 ) );
-	$blue  = hexdec( substr( $hexcolor, 4, 2 ) );
+	$red      = hexdec( substr( $hexcolor, 0, 2 ) );
+	$green    = hexdec( substr( $hexcolor, 2, 2 ) );
+	$blue     = hexdec( substr( $hexcolor, 4, 2 ) );
 
 	$luminosity = ( ( $red * 0.2126 ) + ( $green * 0.7152 ) + ( $blue * 0.0722 ) );
 
--- lib/woocommerce/woocommerce-setup.php
+++ lib/woocommerce/woocommerce-setup.php
@@ -37,3 +37,73 @@ function genesis_sample_woocommerce_styles( $enqueue_styles ) {
 	return $enqueue_styles;
 
 }
+
+add_filter( 'woocommerce_style_smallscreen_breakpoint', 'genesis_sample_woocommerce_breakpoint' );
+/**
+ * Modify the WooCommerce breakpoints for the Genesis Sample Theme.
+ *
+ * @since 1.1.0
+ */
+function genesis_sample_woocommerce_breakpoint() {
+
+	$current = genesis_site_layout();
+	$layouts = array(
+		'one-sidebar' => array(
+			'content-sidebar',
+			'sidebar-content',
+		),
+		'two-sidebar' => array(
+			'content-sidebar-sidebar',
+			'sidebar-content-sidebar',
+			'sidebar-sidebar-content',
+		),
+	);
+
+	if ( in_array( $current, $layouts['two-sidebar'] ) ) {
+		return '2000px'; // Show mobile styles immediately
+	}
+	elseif ( in_array( $current, $layouts['one-sidebar'] ) ) {
+		return '1200px';
+	}
+	else {
+		return '860px';
+	}
+
+}
+
+add_action( 'after_switch_theme', 'genesis_sample_woocommerce_image_dimensions', 1 );
+/**
+* Define WooCommerce image sizes on activation for the Genesis Sample Theme.
+*
+* @since 1.0.0
+*/
+function genesis_sample_woocommerce_image_dimensions() {
+
+	global $pagenow;
+
+	if ( ! isset( $_GET['activated'] ) || $pagenow != 'themes.php' ) {
+		return;
+	}
+
+	$catalog = array(
+		'width'  => '300', // px
+		'height' => '300', // px
+		'crop'   => 1,     // true
+	);
+	$single = array(
+		'width'  => '600', // px
+		'height' => '600', // px
+		'crop'   => 1,     // true
+	);
+	$thumbnail = array(
+		'width'  => '180', // px
+		'height' => '180', // px
+		'crop'   => 1,     // true
+	);
+
+	// Image sizes.
+	update_option( 'shop_catalog_image_size', $catalog );     // Product category thumbs.
+	update_option( 'shop_single_image_size', $single );       // Single product image.
+	update_option( 'shop_thumbnail_image_size', $thumbnail ); // Image gallery thumbs.
+
+}
