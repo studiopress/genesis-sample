6a8be7ef1d009386f1611e95d925ff632764f7cd
Jen Baumann <baumann@dreamwhisperdesigns.com>
2018-02-13 14:55:00 -0500

    Standards

--- lib/theme-defaults.php
+++ lib/theme-defaults.php
@@ -12,10 +12,10 @@
 
 add_filter( 'genesis_theme_settings_defaults', 'genesis_sample_theme_defaults' );
 /**
-* Updates theme settings on reset.
-*
-* @since 2.2.3
-*/
+ * Updates theme settings on reset.
+ *
+ * @since 2.2.3
+ */
 function genesis_sample_theme_defaults( $defaults ) {
 
 	$defaults['blog_cat_num']              = 6;
@@ -31,10 +31,10 @@ function genesis_sample_theme_defaults( $defaults ) {
 
 add_action( 'after_switch_theme', 'genesis_sample_theme_setting_defaults' );
 /**
-* Updates theme settings on activation.
-*
-* @since 2.2.3
-*/
+ * Updates theme settings on activation.
+ *
+ * @since 2.2.3
+ */
 function genesis_sample_theme_setting_defaults() {
 
 	if ( function_exists( 'genesis_update_settings' ) ) {
