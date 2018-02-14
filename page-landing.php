fcfaa7ad6bbbd96c83d4735fb4b73e67873d8fab
Nick Cernis <nick@cern.is>
2018-02-14 16:30:30 +0100

    Standards: landing page name

---
+++ page-landing.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds the landing page template to the Genesis Sample Theme.
+ *
+ * Template Name: Landing
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    https://www.studiopress.com/
+ */
+
+add_filter( 'body_class', 'genesis_sample_add_body_class' );
+/**
+ * Add landing page body class.
+ *
+ * @since 1.0.0
+ *
+ * @param array $classes Original body classes.
+ * @return array Modified body classes.
+ */
+function genesis_sample_add_body_class( $classes ) {
+
+	$classes[] = 'landing-page';
+
+	return $classes;
+
+}
+
+// Remove Skip Links.
+remove_action( 'genesis_before_header', 'genesis_skip_links', 5 );
+
+add_action( 'wp_enqueue_scripts', 'genesis_sample_dequeue_skip_links' );
+/**
+ * Dequeue Skip Links Script.
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_dequeue_skip_links() {
+	wp_dequeue_script( 'skip-links' );
+}
+
+// Force full width content layout.
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+// Remove site header elements.
+remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+
+// Remove navigation.
+remove_theme_support( 'genesis-menus' );
+
+// Remove breadcrumbs.
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+// Remove footer widgets.
+remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+
+// Remove site footer elements.
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+
+// Run the Genesis loop.
+genesis();
