dd3773a031ebaff591d79f66a529bfd799a83af8
Nick Cernis <nick@cern.is>
2018-03-13 19:21:12 +0100

    Logo: adjust margin with JS to fit under header

---
+++ js/genesis-sample.js
@@ -0,0 +1,61 @@
+/**
+ * Genesis Sample entry point.
+ *
+ * @package GenesisSample\JS
+ * @author  StudioPress
+ * @license GPL-2.0+
+ */
+
+var genesisSample = ( function( $ ) {
+	'use strict';
+
+	/**
+	 * Adjust site inner margin top to compensate for sticky header height.
+	 *
+	 * @since 2.4.0
+	 */
+	var moveContentBelowFixedHeader = function() {
+		var siteInnerMarginTop = 0;
+
+		if( $('.site-header').css('position') === 'fixed' ) {
+			siteInnerMarginTop = $('.site-header').outerHeight();
+		}
+
+		$('.site-inner').css('margin-top', siteInnerMarginTop);
+	},
+
+	/**
+	 * Initialize Genesis Sample.
+	 *
+	 * Internal functions to execute on document ready can be called here.
+	 *
+	 * @since 2.4.0
+	 */
+	init = function() {
+		// Run on first load.
+		moveContentBelowFixedHeader();
+
+		// Run after window resize.
+		$( window ).resize(function() {
+			moveContentBelowFixedHeader();
+		});
+
+		// Run after the Customizer updates.
+		// 1.5s delay is to allow logo area reflow.
+		if (typeof wp.customize != "undefined") {
+			wp.customize.bind( 'change', function ( setting ) {
+				setTimeout(function() {
+					moveContentBelowFixedHeader();
+				  }, 1500);
+			});
+		}
+	};
+
+	// Expose the init function only.
+	return {
+		init: init
+	};
+
+})( jQuery );
+
+jQuery( genesisSample.init );
