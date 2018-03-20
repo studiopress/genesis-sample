1646d7548f96f5f767bf245f5580813946d77a25
Jen Baumann <baumann@dreamwhisperdesigns.com>
2018-03-20 13:45:09 -0400

    Simple Social Icon defaults for setup

--- lib/theme-defaults.php
+++ lib/theme-defaults.php
@@ -58,3 +58,31 @@ function genesis_sample_theme_setting_defaults() {
 	update_option( 'posts_per_page', 6 );
 
 }
+
+add_filter( 'simple_social_default_styles', 'genesis_sample_social_default_styles' );
+/**
+ * Set Simple Social Icon defaults.
+ *
+ * @since 1.0.0
+ *
+ * @param array $defaults Social style defaults.
+ * @return array Modified social style defaults.
+ */
+function daily_dish_social_default_styles( $defaults ) {
+
+	$args = array(
+		'alignment'              => 'alignleft',
+		'background_color'       => '#f5f5f5',
+		'background_color_hover' => '#333333',
+		'border_radius'          => 3,
+		'border_width'           => 0,
+		'icon_color'             => '#333333',
+		'icon_color_hover'       => '#ffffff',
+		'size'                   => 40,
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	return $args;
+
+}
