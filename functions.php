130a0ab83ae1e76e0a2f9cd65fb81a525412b588
Brian Gardner <bmg1227@gmail.com>
2018-09-12 14:19:10 -0500

    Add slugs to color palettes.

--- functions.php
+++ functions.php
@@ -213,14 +213,17 @@ add_theme_support( 'editor-font-sizes', array(
 add_theme_support( 'editor-color-palette', array(
 	array(
 		'name'  => __( 'Light gray', 'genesis-sample' ),
+		'slug'  => 'light-gray',
 		'color'	=> '#f5f5f5',
 	),
 	array(
 		'name'  => __( 'Medium gray', 'genesis-sample' ),
+		'slug'  => 'medium-gray',
 		'color' => '#999',
 	),
 	array(
 		'name'  => __( 'Dark gray', 'genesis-sample' ),
+		'slug'  => 'dark-gray',
 		'color' => '#333',
        ),
 ) );
