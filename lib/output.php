8dbf0e39e028163986140b145bdf31169767be1d
Nick Cernis <nick@cern.is>
2018-10-02 14:12:18 +0200

    Fix Gutenburg button hover color

--- lib/output.php
+++ lib/output.php
@@ -76,7 +76,9 @@ function genesis_sample_css() {
 		.button:hover,
 		.genesis-nav-menu > .menu-highlight > a:hover,
 		.genesis-nav-menu > .menu-highlight > a:focus,
-		.genesis-nav-menu > .menu-highlight.current-menu-item > a {
+		.genesis-nav-menu > .menu-highlight.current-menu-item > a,
+		.content .wp-block-button .wp-block-button__link:focus,
+		.content .wp-block-button .wp-block-button__link:hover {
 			background-color: %s;
 			color: %s;
 		}
