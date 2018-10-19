61d73e922d5c37cb7287717f5d204802561b8a95
Nick Cernis <nick@cern.is>
2018-10-19 14:55:08 +0200

    Add npm scripts
    * Add npm scripts for theme automation tasks
    
    * Document npm scripts
    
    * Add lock file
    
    * Omit npm config files during git export

---
+++ .scripts/makepot.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// This script replaces the default use of `wpi18n makepot`, with custom
+// POT file headers. It can be run with `npm run makepot`.
+
+const chalk = require('chalk');
+const wpi18n = require('node-wp-i18n');
+
+console.log(chalk`{cyan Making pot file}...`);
+
+wpi18n.makepot(
+	{
+		domainPath: 'languages',
+		potHeaders: {
+			'poedit': true,
+			'x-poedit-basepath': '..',
+			'report-msgid-bugs-to': 'StudioPress <translations@studiopress.com>',
+			'last-translator': 'StudioPress <translations@studiopress.com>',
+			'language-team': 'StudioPress <translations@studiopress.com>'
+		}
+	}
+).then(
+	(result) => console.log(chalk`{cyan Pot file updated at {bold ${result.domainPath}/${result.potFile}}}`),
+	(err) => console.log(chalk`{red ${err}}`)
+);
