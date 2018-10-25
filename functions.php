5956d3d0a88bed89b9f2ddb446b426d10b881319
Nick Cernis <nick@cern.is>
2018-10-25 07:51:28 +0200

    Add FitVids
    Solves #126.

--- functions.php
+++ functions.php
@@ -87,9 +87,17 @@ function genesis_sample_enqueue_scripts_styles() {
 	);
 
 	wp_enqueue_script(
+		'fitvids',
+		get_stylesheet_directory_uri() . '/js/jquery.fitvids.min.js',
+		array( 'jquery' ),
+		'1.1',
+		true
+	);
+
+	wp_enqueue_script(
 		'genesis-sample',
 		get_stylesheet_directory_uri() . '/js/genesis-sample.js',
-		array( 'jquery' ),
+		array( 'jquery', 'fitvids' ),
 		CHILD_THEME_VERSION,
 		true
 	);
