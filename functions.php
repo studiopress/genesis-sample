54ebc8c03b41ceff983d4dbe0d7fbc6c3b56830c
Nick Cernis <nick@cern.is>
2018-10-31 10:27:25 +0100

    Move content_width override to Gutenberg directory
    Goal is to allow site owners on previous versions of the theme to
    add Gutenberg support by copying one folder to their theme directory
    and adding the include for init.php to their functions.php file.

--- functions.php
+++ functions.php
@@ -128,17 +128,6 @@ function genesis_sample_responsive_menu_settings() {
 
 }
 
-add_action( 'after_setup_theme', 'genesis_sample_content_width', 0 );
-/**
- * Set content width to match the “wide” Gutenberg block width.
- */
-function genesis_sample_content_width() {
-
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound -- See https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/924
-	$GLOBALS['content_width'] = apply_filters( 'genesis_sample_content_width', 1062 );
-
-}
-
 // Adds support for HTML5 markup structure.
 add_theme_support(
 	'html5',
--- lib/gutenberg/init.php
+++ lib/gutenberg/init.php
@@ -117,3 +117,14 @@ add_theme_support(
 		),
 	)
 );
+
+add_action( 'after_setup_theme', 'genesis_sample_content_width', 0 );
+/**
+ * Set content width to match the “wide” Gutenberg block width.
+ */
+function genesis_sample_content_width() {
+
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound -- See https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/924
+	$GLOBALS['content_width'] = apply_filters( 'genesis_sample_content_width', 1062 );
+
+}
