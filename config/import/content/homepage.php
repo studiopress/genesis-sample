a616043098a0f0d6022f26b01aedef137b6f6f64
Nick Cernis <nick@cern.is>
2018-12-19 07:40:57 +0100

    Add CTA block

--- config/import/content/homepage.php
+++ config/import/content/homepage.php
@@ -20,6 +20,10 @@ $genesis_sample_homepage_content = <<<CONTENT
 
 <!-- wp:atomic-blocks/ab-post-grid {"postsToShow":3,"displayPostDate":false,"displayPostAuthor":false,"columns":3,"align":"wide"} /-->
 
+<!-- wp:atomic-blocks/ab-cta {"buttonText":"Learn more","ctaWidth":"full","ctaBackgroundColor":"#202020","ctaTextColor":"#f5f5f5"} -->
+<div style="background-color:#202020;text-align:center" class="wp-block-atomic-blocks-ab-cta alignfull ab-block-cta ab-font-size-20"><div class="ab-cta-content"><h2 class="ab-cta-title ab-font-size-32" style="color:#f5f5f5">Enter your title here</h2><div class="ab-cta-text ab-font-size-20" style="color:#f5f5f5"><p>Some text to support the title.</p></div></div><div class="ab-cta-button"><a href="https://example.com" target="_self" class="ab-button ab-button-shape-rounded ab-button-size-medium" style="color:#ffffff;background-color:#3373dc">Learn more</a></div></div>
+<!-- /wp:atomic-blocks/ab-cta -->
+
 CONTENT;
 
 return $genesis_sample_homepage_content;
