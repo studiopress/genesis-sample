f679f12f6858d19211ee010aa35e6a55d1fd9825
Nick Cernis <nick@cern.is>
2018-12-19 17:56:55 +0100

    Add onboarding config (#153)
    Uses the new onboarding API in Genesis 2.8.0.
    
    When people activate the theme from Appearance → Themes, they will
    be presented with a page that prompts them to set up their homepage.
    
    Proceeding with the setup process will:
    
    - Install Atomic Blocks.
    - Install a block-based homepage based on the content of `config/import/content/homepage.php`.
    - Set the imported page as the static homepage.
    
    Genesis handles the plugin install and page import functionality.
    The child theme just provides a config to dictate which plugins
    and page content to import.
    
    This commit also includes supporting images used on the homepage,
    which are currently hotlinked from the child theme directory rather 
    than imported.

---
+++ template-blocks.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * A template to force full-width and remove the page title.
+ *
+ * Template Name: Blocks
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+
+// Removes Skip Links.
+remove_action( 'genesis_before_header', 'genesis_skip_links', 5 );
+
+add_action( 'wp_enqueue_scripts', 'genesis_sample_dequeue_skip_links' );
+/**
+ * Dequeues Skip Links Script.
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_dequeue_skip_links() {
+
+	wp_dequeue_script( 'skip-links' );
+
+}
+
+// Remove the page title.
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+add_filter( 'genesis_site_title_wrap', 'genesis_sample_h1_for_site_title' );
+/**
+ * Wraps the site title in an h1 element to make up for removing the page title.
+ *
+ * @param string $wrap The original wrap element.
+ * @return string The new wrap element
+ */
+function genesis_sample_h1_for_site_title( $wrap ) {
+	return 'h1';
+}
+
+// Forces full width content layout.
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+// Removes breadcrumbs.
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+// Removes footer widgets.
+remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
+
+// Runs the Genesis loop.
+genesis();
