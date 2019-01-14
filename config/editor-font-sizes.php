92f1647a199eb12007824ee184f2192da4b66dc8
Nick Cernis <nick@cern.is>
2019-01-14 14:53:38 +0100

    Remove editor font sizes shortname (no longer used by Gutenberg)
    Fixes #160.

--- config/editor-font-sizes.php
+++ config/editor-font-sizes.php
@@ -13,25 +13,21 @@
 return array(
 	array(
 		'name'      => __( 'Small', 'genesis-sample' ),
-		'shortName' => __( 'S', 'genesis-sample' ),
 		'size'      => 12,
 		'slug'      => 'small',
 	),
 	array(
 		'name'      => __( 'Normal', 'genesis-sample' ),
-		'shortName' => __( 'M', 'genesis-sample' ),
 		'size'      => 16,
 		'slug'      => 'normal',
 	),
 	array(
 		'name'      => __( 'Large', 'genesis-sample' ),
-		'shortName' => __( 'L', 'genesis-sample' ),
 		'size'      => 20,
 		'slug'      => 'large',
 	),
 	array(
 		'name'      => __( 'Larger', 'genesis-sample' ),
-		'shortName' => __( 'XL', 'genesis-sample' ),
 		'size'      => 24,
 		'slug'      => 'larger',
 	),
