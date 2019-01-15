868ff6facaad5eee8178d1d97f3e2d3cb90d48d9
Nick Cernis <nick@cern.is>
2019-01-15 16:22:17 +0100

    Merge release 2.8.0 into develop branch (#180)
    Update changelog and versions.

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ require_once get_template_directory() . '/lib/init.php';
 // Defines the child theme (do not remove).
 define( 'CHILD_THEME_NAME', 'Genesis Sample' );
 define( 'CHILD_THEME_URL', 'https://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.8.0-dev' );
+define( 'CHILD_THEME_VERSION', '2.8.0' );
 
 // Sets up the Theme.
 require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
