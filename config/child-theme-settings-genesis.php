dfcf263625f292165198ffaa6761a072ba06e6ff
Jen Baumann <baumann@dreamwhisperdesigns.com>
2019-03-07 12:50:44 -0500

    Extract default child theme settings into config files for Genesis 2.9
    closes #186

---
+++ config/child-theme-settings-genesis.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Genesis Sample settings specific to Genesis.
+ *
+ * Used in `config/child-theme-settings.php` and `lib/theme-defaults.php`.
+ *
+ * Can be removed when the Genesis Theme Settings page is removed from WP admin.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+
+return array(
+	'blog_cat_num'              => 6,
+	'breadcrumb_home'           => 0,
+	'breadcrumb_front_page'     => 0,
+	'breadcrumb_posts_page'     => 0,
+	'breadcrumb_single'         => 0,
+	'breadcrumb_page'           => 0,
+	'breadcrumb_archive'        => 0,
+	'breadcrumb_404'            => 0,
+	'breadcrumb_attachment'     => 0,
+	'content_archive'           => 'full',
+	'content_archive_limit'     => 0,
+	'content_archive_thumbnail' => 0,
+	'image_size'                => '',
+	'image_alignment'           => 'alignleft',
+	'posts_nav'                 => 'numeric',
+	'site_layout'               => 'content-sidebar',
+);
---
+++ config/child-theme-settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Genesis Sample theme settings.
+ *
+ * Genesis 2.9+ updates these settings when themes are activated.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+
+return array(
+	GENESIS_SETTINGS_FIELD => genesis_get_config( 'child-theme-settings-genesis' ),
+	'posts_per_page'       => 6,
+);
