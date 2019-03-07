762f1ece2142121601f96ee1ae0cf50390728b8a
Jen Baumann <baumann@dreamwhisperdesigns.com>
2019-03-07 14:16:24 -0500

    Move templates to page-templates folder & rename

--- config/onboarding.php
+++ config/onboarding.php
@@ -14,19 +14,19 @@ return array(
 	'dependencies' => array(
 		'plugins' => array(
 			array(
-				'name'        => __( 'Atomic Blocks', 'genesis-sample' ),
-				'slug'        => 'atomic-blocks/atomicblocks.php',
+				'name' => __( 'Atomic Blocks', 'genesis-sample' ),
+				'slug' => 'atomic-blocks/atomicblocks.php',
 			),
 		),
 	),
-	'content' => array(
+	'content'      => array(
 		'homepage' => array(
 			'post_title'     => 'Homepage',
 			'post_name'      => 'homepage-gutenberg',
 			'post_content'   => require dirname( __FILE__ ) . '/import/content/homepage.php',
 			'post_type'      => 'page',
 			'post_status'    => 'publish',
-			'page_template'  => 'template-blocks.php',
+			'page_template'  => 'page-templates/blocks.php',
 			'comment_status' => 'closed',
 			'ping_status'    => 'closed',
 		),
--- lib/gutenberg/init.php
+++ lib/gutenberg/init.php
@@ -59,7 +59,7 @@ add_filter( 'body_class', 'genesis_sample_blocks_body_classes' );
  */
 function genesis_sample_blocks_body_classes( $classes ) {
 
-	if ( ! is_singular() || ! function_exists( 'has_blocks' ) || ! function_exists( 'parse_blocks') ) {
+	if ( ! is_singular() || ! function_exists( 'has_blocks' ) || ! function_exists( 'parse_blocks' ) ) {
 		return $classes;
 	}
 
