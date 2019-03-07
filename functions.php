a4fe4a8db3b2d72a44c81434b8fc3f5c8fba8530
Jen Baumann <baumann@dreamwhisperdesigns.com>
2019-03-07 11:52:40 -0500

    Setup 2.9.0 development cycle

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ require_once get_template_directory() . '/lib/init.php';
 // Defines the child theme (do not remove).
 define( 'CHILD_THEME_NAME', 'Genesis Sample' );
 define( 'CHILD_THEME_URL', 'https://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.8.1-dev' );
+define( 'CHILD_THEME_VERSION', '2.9.0-dev' );
 
 // Sets up the Theme.
 require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
@@ -57,7 +57,8 @@ add_action( 'after_setup_theme', 'genesis_child_gutenberg_support' );
  *
  * @since 2.7.0
  */
-function genesis_child_gutenberg_support() { // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- using same in all child themes to allow action to be unhooked.
+function genesis_child_gutenberg_support() {
+ // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- using same in all child themes to allow action to be unhooked.
 	require_once get_stylesheet_directory() . '/lib/gutenberg/init.php';
 }
 
