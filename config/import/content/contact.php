e0e38e5a790d26958fab92852f0aacebec313972
Nathan Rice <ncrice@gmail.com>
2019-03-18 11:44:05 -0400

    Create WP Forms contact form and use it in the contact template.

--- config/import/content/contact.php
+++ config/import/content/contact.php
@@ -12,6 +12,20 @@
  * @link    https://www.studiopress.com/
  */
 
+if ( function_exists( 'wpforms' ) ) {
+	$form_id = wpforms()->form->add(
+		'Simple Contact Form',
+		array(),
+		array(
+		    'template' => 'contact',
+		)
+	);
+
+	if ( $form_id ) {
+		return "<!-- wp:wpforms/form-selector {\"formId\":\"{$form_id}\"} /-->";
+	}
+}
+
 return <<<CONTENT
 <!-- wp:paragraph -->
 <p>Add a contact form to this page with the pre-installed WPForms Lite plugin (Third Party). Learn <a href="https://my.studiopress.com/documentation/wpforms/plugin-usage/create-contact-forms-with-wpforms/" target="_blank" rel="noreferrer noopener" aria-label=" (opens in a new tab)">how to create a form using WPForms</a>.</p>
