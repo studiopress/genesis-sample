dca42e92ec1a7529ef6f86acae9dfa9e71b47b94
Nick Cernis <nick@cern.is>
2019-04-18 21:22:13 +0200

    Use 'menu buttons' to reflect that there can be more than one of them
    Props @susannelson.
    
    https://github.com/studiopress/genesis-sample/issues/220#issuecomment-484617633

--- lib/customize.php
+++ lib/customize.php
@@ -54,7 +54,7 @@ function genesis_sample_customizer_register( $wp_customize ) {
 			$wp_customize,
 			'genesis_sample_accent_color',
 			array(
-				'description' => __( 'Change the default hover color for button links, the menu button, and submit buttons. The button block uses the Link Color.', 'genesis-sample' ),
+				'description' => __( 'Change the default hover color for button links, menu buttons, and submit buttons. The button block uses the Link Color.', 'genesis-sample' ),
 				'label'       => __( 'Accent Color', 'genesis-sample' ),
 				'section'     => 'colors',
 				'settings'    => 'genesis_sample_accent_color',
