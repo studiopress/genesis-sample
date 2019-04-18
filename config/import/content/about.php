fdc39917f7e78b7c3e96e5801807556c1f1a91b4
Nick Cernis <nick@cern.is>
2019-04-18 18:03:54 +0200

    Remove spacers and titles from block content
    This prevents duplication/extra space now that the pages will show
    the regular page titles instead.

--- config/import/content/about.php
+++ config/import/content/about.php
@@ -16,10 +16,6 @@
 $genesis_sample_about_image_url = CHILD_URL . '/config/import/images/about.jpg';
 
 return <<<CONTENT
-<!-- wp:atomic-blocks/ab-spacer {"spacerHeight":70} -->
-<div style="color:#ddd" class="wp-block-atomic-blocks-ab-spacer ab-block-spacer ab-divider-solid ab-divider-size-1"><hr style="height:70px"/></div>
-<!-- /wp:atomic-blocks/ab-spacer -->
-
 <!-- wp:image {"id":2141,"align":"center"} -->
 <div class="wp-block-image"><figure class="aligncenter"><img src="$genesis_sample_about_image_url" alt="" class="wp-image-2141"/></figure></div>
 <!-- /wp:image -->
@@ -30,11 +26,7 @@ return <<<CONTENT
 
 <!-- wp:columns -->
 <div class="wp-block-columns has-2-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":1} -->
-<h1>About Us</h1>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
+<div class="wp-block-column"><!-- wp:paragraph -->
 <p>Hello! We are StudioPress, and  we build themes with an emphasis on typography, white space, and   mobile-optimized design to make your website look absolutely   breathtaking.  </p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
@@ -50,7 +42,7 @@ return <<<CONTENT
 
 <!-- wp:paragraph {"align":"right"} -->
 <p style="text-align:right">
-1234 Block Blvd.<br>San Francisco, CA 94120 
+1234 Block Blvd.<br>San Francisco, CA 94120
 
 </p>
 <!-- /wp:paragraph --></div>
