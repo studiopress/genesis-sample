58affbe69004a6913684de2a677ebcdda1e20496
Nick Cernis <nick@cern.is>
2019-04-18 16:55:41 +0200

    Refactor to remove unneeded studiopress_get_wpforms_id function
    The ID can be retrieved from the genesis_onboarding_wpforms_id.

--- lib/wpforms.php
+++ lib/wpforms.php
@@ -2,7 +2,7 @@
 /**
  * StudioPress WPForms helper functions.
  *
- * Adds a form during one-click theme setup, or returns existing forms ID.
+ * Creates a form during one-click theme setup if one has not been created already.
  *
  * @package StudioPress
  * @author  StudioPress
@@ -11,44 +11,9 @@
  */
 
 /**
- * Gets the ID of the WPForms form created during previous StudioPress one-click theme setup, if one exists.
- *
- * Creates a new form and returns that form's ID if there is no existing form, and `$create_new` is true.
+ * Creates a WPForms form if one added by a StudioPress theme does not exist.
  *
  * @since 2.10.0
- *
- * @param bool $create_new Create a form if no form ID exists and return the ID of that form.
- * @return int|null The ID of the form, or null if WPForms is inactive or form creation failed.
- */
-function studiopress_get_wpforms_id( $create_new = true ) { // phpcs:ignore -- studiopress prefix for functions shared between themes.
-
-	if ( ! function_exists( 'wpforms' ) ) {
-		return;
-	}
-
-	$form_id = get_option( 'genesis_onboarding_wpforms_id' );
-	$wpform  = get_post( $form_id );
-
-	if ( $wpform && 'wpforms' === $wpform->post_type ) {
-		return $form_id;
-	} else {
-		delete_option( 'genesis_onboarding_wpforms_id' ); // Stored ID no longer points to a WPForms form.
-	}
-
-	if ( $create_new ) {
-		return studiopress_create_wpforms_form();
-	}
-
-}
-
-/**
- * Creates a WPForms form.
- *
- * A `wpforms.php` config file must exist in the theme's config folder.
- *
- * @since 2.10.0
- *
- * @return int|null The form/post ID, or null if no form was created or form config is missing.
  */
 function studiopress_create_wpforms_form() { // phpcs:ignore -- studiopress prefix for functions shared between themes.
 
@@ -56,27 +21,32 @@ function studiopress_create_wpforms_form() { // phpcs:ignore -- studiopress pref
 		return;
 	}
 
-	$wpforms_data = genesis_get_config( 'wpforms' );
+	$existing_form_id = get_option( 'genesis_onboarding_wpforms_id' );
 
-	if ( ! $wpforms_data ) {
-		return;
+	if ( $existing_form_id ) {
+		$wpform = get_post( $existing_form_id );
+
+		// Don't create another form if a valid one already exists.
+		if ( $wpform && 'wpforms' === $wpform->post_type ) {
+			return;
+		}
+
+		// Stored ID no longer points to a WPForms form.
+		delete_option( 'genesis_onboarding_wpforms_id' );
 	}
 
-	// Creates an empty form so we have an ID to work with.
-	$form_id = wp_insert_post(
+	// Creates a form using the WPForms 'contact' template.
+	$new_form_id = wpforms()->form->add(
+		esc_html__( 'Simple Contact Form', 'genesis-sample' ),
+		array(),
 		array(
-			'post_status' => 'publish',
-			'post_type'   => 'wpforms',
+			'template' => 'contact',
+			'builder'  => false,
 		)
 	);
 
-	$wpforms_data['id'] = $form_id; // WPForms expects the ID in the form's post content.
-
-	$wpforms_updated = wpforms()->form->update( $form_id, $wpforms_data );
-
-	if ( $form_id && $wpforms_updated ) {
-		update_option( 'genesis_onboarding_wpforms_id', $form_id, false );
-		return $form_id;
+	if ( $new_form_id ) {
+		update_option( 'genesis_onboarding_wpforms_id', $new_form_id, false );
 	}
 
 }
