a4d6ad0c1df5af0966741734bf616d1100e2b58f
Nick Cernis <nick@cern.is>
2019-07-25 18:34:03 +0100

    Add footer widgets to onboarding config (#277)
    * Add footer widgets to onboarding config
    
    Ensures footer widgets are imported during one-click theme setup.
    
    Dependent on widget import being implemented in Genesis 3.1.
    See https://github.com/studiopress/genesis/issues/2141.
    
    Fixes #276.
    
    * Enable visual mode for text widgets
    
    Prevents an issue where imported widget content would appear as plain
    text, with a message above the widget in the admin area suggesting
    conversion to a Custom HTML widget.

--- config/onboarding.php
+++ config/onboarding.php
@@ -101,4 +101,39 @@ return array(
 			),
 		),
 	),
+	'widgets'          => array(
+		'footer-1' => array(
+			array(
+				'type' => 'text',
+				'args' => array(
+					'title'  => 'Design',
+					'text'   => '<p>With an emphasis on typography, white space, and mobile-optimized design, your website will look absolutely breathtaking.</p><p><a href="#">Learn more about design</a>.</p>',
+					'filter' => 1,
+					'visual' => 1,
+				),
+			),
+		),
+		'footer-2' => array(
+			array(
+				'type' => 'text',
+				'args' => array(
+					'title'  => 'Content',
+					'text'   => '<p>Our team will teach you the art of writing audience-focused content that will help you achieve the success you truly deserve.</p><p><a href="#">Learn more about content</a>.</p>',
+					'filter' => 1,
+					'visual' => 1,
+				),
+			),
+		),
+		'footer-3' => array(
+			array(
+				'type' => 'text',
+				'args' => array(
+					'title'  => 'Strategy',
+					'text'   => '<p>We help creative entrepreneurs build their digital business by focusing on three key elements of a successful online platform.</p><p><a href="#">Learn more about strategy</a>.</p>',
+					'filter' => 1,
+					'visual' => 1,
+				),
+			),
+		),
+	),
 );
