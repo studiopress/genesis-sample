eb19db3d15f13ec6c395ecc8ff7ae4ce8aead7ca
Jen Baumann <adreamwhisperer@gmail.com>
2019-07-31 12:01:34 -0400

    Add featured image support for singular posts and pages
    - Adds image size
    - Adds config file for post type supports (reflecting how we're handling theme supports)

---
+++ config/post-type-supports.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Genesis Sample child theme.
+ *
+ * Post type supports.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://my.studiopress.com/themes/genesis-sample/
+ */
+
+return array(
+	'post' => array(
+		'genesis-singular-images',
+	),
+	'page' => array(
+		'genesis-singular-images',
+	),
+);
