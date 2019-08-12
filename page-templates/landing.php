1989b92fc9244acc3adc62e3c79e970a372bbd3e
Jen Baumann <adreamwhisperer@gmail.com>
2019-08-12 09:26:19 -0400

    Allow alternate layouts on landing page

--- page-templates/landing.php
+++ page-templates/landing.php
@@ -43,9 +43,6 @@ function genesis_sample_dequeue_skip_links() {
 
 }
 
-// Forces full width content layout.
-add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
-
 // Removes site header elements.
 remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
 remove_action( 'genesis_header', 'genesis_do_header' );
